e46c6d46f221e7dccde00c21c5e1bacd04b99485
Wail Yousif <wail.mohamed@sydney-tech.com>
2020-12-14 19:53:30 +0200

    Adding src

---
+++ src/com/company/firstapp/MainClass.java
@@ -0,0 +1,10 @@
+package com.company.firstapp;
+
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello Abdelrahman");
+    }
+}
